be0e18cb0bad640fb2c9bccf71002b7827169ee6
Diego Damaceno dos Santos <78373208+diego780705@users.noreply.github.com>
2022-05-22 13:38:39 -0300

    Update InstalacaoUbuntu.sh

--- InstalacaoUbuntu.sh
+++ InstalacaoUbuntu.sh
@@ -47,6 +47,14 @@ dpkg -i homeassistant-supervised.deb
 
 sleep 2
 
+docker volume create portainer_data
+
+sleep 2
+
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
+sleep 2
+
 systemctl enable firewalld
 
 sleep 2
